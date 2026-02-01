72114a62b6f532e183fda9eda49f6831f891eb2c
iamwillogutu <iamwill.ogutu@gmail.com>
2026-02-01 21:18:48 +0300

    Recipe App

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Recipe Sharing App</h1>
+
+      {/* Form to add a new recipe */}
+      <AddRecipeForm />
+
+      {/* List of recipes */}
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
