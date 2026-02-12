3638fdcceb8994f213b368a3ac9c6bc97812fdb9
iamwillogutu <iamwill.ogutu@gmail.com>
2026-02-13 02:30:35 +0300

    User search application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Search() {
+  return <h2>Search GitHub Users</h2>
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <header>
+          <h1>GitHub User Search</h1>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/search">Search</Link>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
